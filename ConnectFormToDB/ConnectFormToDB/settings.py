6664c5f2621e9b9ebdf821c0584a078011cba9e9
mahbubrahman1 <mahbuburrofficial@gmail.com>
2022-06-18 18:25:06 +0600

    updated via all files

--- ConnectFormToDB/ConnectFormToDB/settings.py
+++ ConnectFormToDB/ConnectFormToDB/settings.py
@@ -14,7 +14,6 @@ import os
 
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 TEMPLATES_DIR = os.path.join(BASE_DIR, 'templates')
-STATIC_DIR = os.path.join(BASE_DIR, 'static')
 
 
 # Quick-start development settings - unsuitable for production
@@ -117,10 +116,7 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.0/howto/static-files/
 
-STATIC_URL = '/static/'
-STATICFILES_DIRS = [
-    STATIC_DIR,
-]
+STATIC_URL = 'static/'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
--- ConnectFormToDB/ConnectFormToDB/urls.py
+++ ConnectFormToDB/ConnectFormToDB/urls.py
@@ -2,6 +2,8 @@ from django.contrib import admin
 from django.urls import path
 from MyApp import views
 
+app_name = 'my_app'
+
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', views.home, name='home'),

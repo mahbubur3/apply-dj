0be7ffa0425c3d133fdc547aa20238d196e48d33
mahbubrahman1 <mahbuburrofficial@gmail.com>
2022-06-26 16:25:58 +0600

    updated

--- SampleProject/MyApp/views.py
+++ SampleProject/MyApp/views.py
@@ -2,7 +2,6 @@ from django.shortcuts import render
 from django.http import HttpResponse
 from MyApp import forms
 from MyApp.models import Actor, Movie
-from django.db.models import Avg
 
 
 def home(request):
@@ -18,13 +17,10 @@ def movie_list(request, actor_id):
     actor_info = Actor.objects.get(pk=actor_id)
     movie_list = Movie.objects.filter(
         actor=actor_id).order_by('movie_name', 'release_date')
-    actor_rating = Movie.objects.filter(
-        actor=actor_id).aggregate(Avg('rating_star)'))
     data = {
         'title': "Movies",
         'actor_info': actor_info,
         'movie_list': movie_list,
-        'actor_rating': actor_rating,
     }
     return render(request, 'my_app/movie_list.html', context=data)
 

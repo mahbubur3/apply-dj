d206f4be1ee51ec6350308de4907d3b0c8603b5d
mahbubrahman1 <mahbuburrofficial@gmail.com>
2022-06-28 17:01:44 +0600

    finished project

--- SampleProject/MyApp/urls.py
+++ SampleProject/MyApp/urls.py
@@ -9,4 +9,7 @@ urlpatterns = [
     path('addactor/', views.add_actor_form, name="add_actor_form"),
     path('movielist/<int:actor_id>/', views.movie_list, name="movie_list"),
     path('editactor/<int:actor_id>/', views.edit_actor, name="edit_actor"),
+    path('edit_movie/<int:movie_id>/', views.edit_movie, name="edit_movie"),
+    path('deletemovie/<int:movie_id>/', views.delete_movie, name="delete_movie"),
+    path('deleteactor/<int:actor_id>/', views.delete_actor, name="delete_actor"),
 ]
--- SampleProject/MyApp/views.py
+++ SampleProject/MyApp/views.py
@@ -62,5 +62,41 @@ def add_movie_form(request):
 def edit_actor(request, actor_id):
     actor_info = Actor.objects.get(pk=actor_id)
     form = forms.ActorForm(instance=actor_info)
+
+    if request.method == 'POST':
+        form = forms.ActorForm(request.POST, instance=actor_info)
+
+        if form.is_valid():
+            form.save(commit=True)
+            return movie_list(request, actor_id)
+
     data = {'edit_form': form}
     return render(request, 'my_app/edit_actor.html', context=data)
+
+
+def edit_movie(request, movie_id):
+    movie_info = Movie.objects.get(pk=movie_id)
+    form = forms.MovieForm(instance=movie_info)
+    data = {}
+    if request.method == 'POST':
+        form = forms.MovieForm(request.POST, instance=movie_info)
+
+        if form.is_valid():
+            form.save(commit=True)
+            data.update({'success_text': 'Updated'})
+
+    data.update({'edit_form': form})
+    data.update({'movie_id': movie_id})
+    return render(request, 'my_app/edit_movie.html', context=data)
+
+
+def delete_movie(request, movie_id):
+    form = Movie.objects.get(pk=movie_id).delete()
+    data = {'delete_success': 'Deleted!'}
+    return render(request, 'my_app/delete.html', context=data)
+
+
+def delete_actor(request, actor_id):
+    actor = Actor.objects.get(pk=actor_id).delete(0)
+    data = {'delete_success': 'Deleted!'}
+    return render(request, 'my_app/delete.html', context=data)
